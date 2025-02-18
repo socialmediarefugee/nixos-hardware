a65c4c9d31e12244014995ad5dcf49733bfb45d9
Nunya Bidness <1012004+socialmediarefugee@users.noreply.github.com>
2025-02-18 01:22:07 -0600

    Added prime support to 15-ek1013dx

--- omen/15-ek1013dx/default.nix
+++ omen/15-ek1013dx/default.nix
@@ -3,11 +3,22 @@
 {
   imports = [
     ../../common/cpu/intel/comet-lake
+    ../../common/gpu/nvidia/prime.nix
     ../../common/gpu/nvidia/ampere
     ../../common/pc/laptop
     ../../common/pc/laptop/ssd
-    ../../common/pc/laptop/hdd
   ];
 
-  boot.kernelModules = [ "hp-wmi" ];
+  # Enables ACPI platform profiles
+  boot = lib.mkIf (lib.versionAtLeast pkgs.linux.version "6.1") {
+    kernelModules = [ "hp-wmi" ];
+  };
+
+  # Enables Wifi and Bluetooth
+  hardware.enableRedistributableFirmware = true
+
+  hardware.nvidia.prime = {
+    intelBusId = "PCI:0:2:0";
+    nvidiaBusId = "PCI:1:0:0";
+  }
 }
